373cdbc496f5c92e1dd7b56bc68738eed108d146
bwrosen20 <bwrosen20@gmail.com>
2022-12-28 16:33:41 -0500

    commit tree 57fd4a11c130d6ebe4ec02860086e6c9d0681cab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers,variable){
+    let answer=drivers.filter(drivers => (drivers===variable)||(drivers===variable.toLowerCase()));
+    return answer;
+}
+
+function fuzzyMatch(drivers,variable){
+   let answer= (filterItems(drivers,variable));
+   return answer;
+
+
+function filterItems(arr,query)
+{
+    return arr.filter((el) => el.toLowerCase().startsWith(query.toLowerCase()));
+}}
+
+function matchName(drivers,variable){
+    let answer=drivers.filter(drivers=>drivers.name===variable);
+    return answer;
+}
